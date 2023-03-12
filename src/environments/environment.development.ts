ee6de752ca33f35d3fcd808f7f9bebde22cb63c8
Matteo <pallottimatteo87@gmail.com>
2023-03-12 17:32:30 +0100

    cambio modalita prod per environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -6,6 +6,5 @@ export const environment = {
     techUrl: 'http://localhost:3000/tech',
     hardwaresUrl: 'http://localhost:3000/hardwares',
     interventiUrl: 'http://localhost:3000/interventi',
-    tokenPB: '-----BEGIN PUBLIC KEY-----MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEA1xgE38q6MYbbxbkW8bm/9DrMTo/XiZkhPKEvQaKnMJFj9puygWAChu9KDC+xlwgAgAbRSeiPYxkUxFnZJJ0Ewd77ykLYR5hFTYnS++Ozt8Z3+tPQB7LWBZWn/WyxM/cDXn5P+hMJJEbZuXDWE/Hj0UuisxEL7qRVPrDZVzlKfWybSrY8t2/QdByrRh9Fzf77tS/xWSMvqV/+L3WRzWKw30GD2F8J6Dgu05ORKrj5c1nOtmqvB7Lo8oEhLxqPCyL1x55ekOQxlc4BeixzINvn0+LyaGKv2Y2ll350VYC6Y+Axmx1cOvljx0Kt+fUSPSX1ssjeNVn0KD/6Mr4NRSIyN5QC90D10HAouugEdnEMPwudaLVBVQcoRePlbeTdKTVlT2dGgWQ4CrdzVjoYbORXJvfWax2/wiKXU6/EyyPfWPmDPGss+x7YAb9dfYb7KR2ekqUH/tF4ESrg/NbRomtIrKoN0NZX12htDwVepTnXnhsSa2RgT7Sii0DJIllKwIbhIaXMrdmaxu5eCpDA4Dgz6qddVJIPBB2zCihK3vESufkI7TvXsVe5ue3tM7tEeZPqYctuBUl3G1/uf5YO3KB0houLHtvpqHVk+AH+aC+ZtoXC5mSuqKT1Be/+odorlJSQQpMwfKYin/7Tge3nJNW+eKCr42zJ68VspSKobM5MjacCAwEAAQ==-----END PUBLIC KEY-----',
     production: false
 };
