0d4e395bd06e6724e0d27c2f0cc8580fd8e42eba
ILMaverick <pallottimatteo@gmail.com>
2023-03-12 21:59:51 +0100

    rework autorizzazioni

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { inject, NgModule } from '@angular/core';
-import { RouterModule, Routes, ActivatedRoute, ParamMap } from '@angular/router';
+import { RouterModule, Routes, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { PaginaPrincipaleComponent } from './components/pagina-principale/pagina-principale.component';
 import { PaginaNonTrovataComponent } from './components/pagina-non-trovata/pagina-non-trovata.component';
 import { PaginaRegistrazioneComponent } from './components/pagina-registrazione/pagina-registrazione.component';
-import { PaginaRecuperoCredenzialiComponent } from './components/pagina-recupero-credenziali/pagina-recupero-credenziali.component';
 import { UtenteAutenticatoGuard } from './services/guards/utente-autenticato.guard';
 import { UtenteAutorizzatoGuard } from './services/guards/utente-autorizzato.guard';
-import { TechAutorizzatoGuard } from './services/guards/tech-autorizzato.guard';
 import { PaginaUtentiComponent } from './components/pagina-utenti/pagina-utenti.component';
 import { PaginaUtenteComponent } from './components/pagina-utenti/pagina-utente/pagina-utente.component';
 import { PaginaInCostruzioneComponent } from './components/pagina-in-costruzione/pagina-in-costruzione.component';
@@ -22,36 +20,36 @@ const routes: Routes = [
   { path: 'chiSiamo', component: PaginaInCostruzioneComponent },
   { path: 'contatti', component: PaginaInCostruzioneComponent },
   {
-    path: 'utenti', canActivate: [() => inject(UtenteAutenticatoGuard).canActivate()], children: [{
+    path: 'utenti', canActivate: [() => inject(UtenteAutenticatoGuard).canActivate()], canActivateChild:[(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => inject(UtenteAutorizzatoGuard).canActivate(route, state)], children: [{
       path: '', pathMatch: 'full', redirectTo: '/home'
     },
     {
-      path: 'all', canActivate: [() => inject(TechAutorizzatoGuard).canActivate()], component: PaginaUtentiComponent
+      path: 'all', component: PaginaUtentiComponent
     },
     {
-      path: ':id', canActivate: [() => inject(UtenteAutorizzatoGuard).canActivate()], component: PaginaUtenteComponent
+      path: ':id', component: PaginaUtenteComponent
     }]
   },
   {
-    path: 'hardware', canActivate: [() => inject(UtenteAutenticatoGuard).canActivate()], children: [{
-      path: '', canActivate: [() => inject(TechAutorizzatoGuard).canActivate()], component: PaginaInCostruzioneComponent
+    path: 'hardware', canActivate: [() => inject(UtenteAutenticatoGuard).canActivate()], canActivateChild:[(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => inject(UtenteAutorizzatoGuard).canActivate(route, state)],children: [{
+      path: '', component: PaginaInCostruzioneComponent
     },
-    { path: 'all', canActivate: [() => inject(UtenteAutorizzatoGuard).canActivate()], component: PaginaInCostruzioneComponent },
-    { path: ':id_hardware', canActivate: [() => inject(UtenteAutorizzatoGuard).canActivate()], component: PaginaInCostruzioneComponent }]
+    { path: 'all', component: PaginaInCostruzioneComponent },
+    { path: ':id_hardware', component: PaginaInCostruzioneComponent }]
   },
   {
-    path: 'interventi', canActivate: [() => inject(UtenteAutenticatoGuard).canActivate()], children: [{
-      path: '', canActivate: [() => inject(TechAutorizzatoGuard).canActivate()], component: PaginaInCostruzioneComponent
+    path: 'interventi', canActivate: [() => inject(UtenteAutenticatoGuard).canActivate()], canActivateChild:[(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => inject(UtenteAutorizzatoGuard).canActivate(route, state)],children: [{
+      path: '', component: PaginaInCostruzioneComponent
     },
-    { path: 'all', canActivate: [() => inject(UtenteAutorizzatoGuard).canActivate()], component: PaginaInCostruzioneComponent },
-    { path: ':id_intervento', canActivate: [() => inject(UtenteAutorizzatoGuard).canActivate()], component: PaginaInCostruzioneComponent }]
+    { path: 'all', component: PaginaInCostruzioneComponent },
+    { path: ':id_intervento', component: PaginaInCostruzioneComponent }]
   },
   {
-    path: 'tickets', canActivate: [() => inject(UtenteAutenticatoGuard).canActivate()], children: [{
-      path: '', canActivate: [() => inject(TechAutorizzatoGuard).canActivate()], component: PaginaInCostruzioneComponent
+    path: 'tickets', canActivate: [() => inject(UtenteAutenticatoGuard).canActivate()], canActivateChild:[(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => inject(UtenteAutorizzatoGuard).canActivate(route, state)],children: [{
+      path: '', component: PaginaInCostruzioneComponent
     },
-    { path: 'all', canActivate: [() => inject(UtenteAutorizzatoGuard).canActivate()], component: PaginaInCostruzioneComponent },
-    { path: ':id_tickets', canActivate: [() => inject(UtenteAutorizzatoGuard).canActivate()], component: PaginaInCostruzioneComponent }]
+    { path: 'all', component: PaginaInCostruzioneComponent },
+    { path: ':id_tickets', component: PaginaInCostruzioneComponent }]
   },
   { path: 'supportoUtente', canActivate: [() => inject(UtenteAutenticatoGuard).canActivate()], component: PaginaInCostruzioneComponent },
   { path: '404', component: PaginaNonTrovataComponent },
--- src/app/services/guards/utente-autorizzato.guard.ts
+++ src/app/services/guards/utente-autorizzato.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Router, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AutenticazioneService } from '../autenticazione.service';
 import { UtentiService } from '../utenti.service';
@@ -7,16 +7,15 @@ import { UtentiService } from '../utenti.service';
 @Injectable({
   providedIn: 'root'
 })
-export class UtenteAutorizzatoGuard {
-
-  idUrl!: string;
+export class UtenteAutorizzatoGuard{
 
   constructor(private _utente: UtentiService, private _auth: AutenticazioneService, private _route: Router) {
     
   }
 
-  canActivate(): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    if (this.idUrl === this._utente.getUserIdFromLocal()) {
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    
+    if (route.params['id'] && route.params['id'] !== this._utente.getUserIdFromLocal() && !this._utente.getRoleFromLocal().match('tecnico')) {
       alert('Utente non autorizzato o sessione scaduta. Effettuare il Login!');
       return this._route.createUrlTree(['home']);
     } else {
